8f4023b913f1e579c721ec7594fc08688c9841a8
Zabil Cheriya Maliackal <zabilcm@gmail.com>
2024-06-03 15:59:17 +0100

    Add non recursive logic and format
    Signed-off-by: Zabil Cheriya Maliackal <zabilcm@gmail.com>

--- src/utils/Util.ts
+++ src/utils/Util.ts
@@ -11,17 +11,14 @@ export type CommonAsyncFunction<T = unknown> = (
 
 // biome-ignore lint/complexity/noStaticOnlyClass: <explanation>
 export class Util {
-  public static async importFile(file: string): Promise<unknown> {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+  public static async importFile(file: string) {
     return await import(file);
   }
 
-  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   public static writeFile(filePath: string, content: string) {
     writeFileSync(filePath, content);
   }
 
-  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   public static exists(filePath: string) {
     return existsSync(filePath);
   }
@@ -30,11 +27,11 @@ export class Util {
     return readFileSync(file, "utf-8");
   }
 
-  public static readFileBuffer(file: string): ArrayBuffer {
+  public static readFileBuffer(file: string) {
     return readFileSync(file).buffer;
   }
 
-  public static getListOfFiles(): Array<string> {
+  public static getListOfFiles() {
     return Util.getImplDirs().reduce((files: Array<string>, dir) => {
       if (!existsSync(dir)) {
         console.log(`Failed to load implementations from ${dir}`);
@@ -67,14 +64,14 @@ export class Util {
 
   //   return fileList;
   // }
-  public static collectFilesIn(dir: string): string[] {
+  public static collectFilesIn(dir: string) {
     return klawSync(dir, {
       filter: (item) => Util.isTSFile(item.path),
       traverseAll: true,
     }).map((item) => item.path);
   }
 
-  public static getImplDirs(): Array<string> {
+  public static getImplDirs() {
     const projectRoot = process.env.GAUGE_PROJECT_ROOT as string;
 
     if (process.env.STEP_IMPL_DIR) {
@@ -86,25 +83,24 @@ export class Util {
     return [join(projectRoot, "tests")];
   }
 
-  public static getNewTSFileName(dir: string, counter = 0): string {
-    const tmpl =
-      (counter && `StepImplementation${counter}${Extension.Ts}`) ||
-      "StepImplementation.ts";
-    const fileName = join(dir, tmpl);
+  public static getNewTSFileName(dir: string) {
+    let counter = 0;
+    let fileName: string;
 
-    if (!existsSync(fileName)) {
-      return fileName;
-    }
+    do {
+      const tmpl = `StepImplementation${counter || ""}${Extension.Ts}`;
+      fileName = join(dir, tmpl);
+      counter++;
+    } while (existsSync(fileName));
 
-    return Util.getNewTSFileName(dir, counter + 1);
+    return fileName;
   }
 
-  public static isAsync(m: CommonFunction): boolean {
-    // eslint-disable-next-line @typescript-eslint/no-empty-function
+  public static isAsync(m: CommonFunction) {
     return m instanceof (async () => {}).constructor;
   }
 
-  public static getUniqueScreenshotFileName(): string {
+  public static getUniqueScreenshotFileName() {
     const dir = (process.env.gauge_screenshots_dir as string) ?? "";
 
     return join(dir, `screenshot-${v4()}.png`);
