c5a717e2db6682bfc75bf7918939ddff7b0341ff
Zabil Cheriya Maliackal <zabilcm@gmail.com>
2024-07-01 14:50:10 +0100

    Fix function name typo
    Signed-off-by: Zabil Cheriya Maliackal <zabilcm@gmail.com>

--- e2e/tests/parameter.ts
+++ e2e/tests/parameter.ts
@@ -3,7 +3,7 @@ import { Step } from "gauge-ts";
 
 export default class Parameter {
   @Step("Trimmed <original> is the same as <expected>")
-  public async checkTrimmeWord(original: string, expected: string) {
+  public async checkTrimmedWord(original: string, expected: string) {
     assert.strictEqual(original.trim(), expected);
   }
 }
