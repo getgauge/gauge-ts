5c752a1360d42f41a440274dab3046dee499132e
Zabil Cheriya Maliackal <zabilcm@gmail.com>
2024-07-01 16:21:14 +0100

    Move parameter spec implementation
    Signed-off-by: Zabil Cheriya Maliackal <zabilcm@gmail.com>

--- e2e/tests/parameter.ts
+++ e2e/tests/parameter.ts
@@ -1,9 +1,17 @@
 import * as assert from "node:assert";
+import type { Person } from "@lib/Person";
 import { Step } from "gauge-ts";
 
 export default class Parameter {
   @Step("Trimmed <original> is the same as <expected>")
   public async checkTrimmedWord(original: string, expected: string) {
     assert.strictEqual(original.trim(), expected);
+  }
+
+  @Step("This step uses a custom parameter of type Person and value <person>")
+  public async validatePerson(person: Person) {
+    assert.strictEqual(person.name, "John");
+    assert.strictEqual(person.age, 30);
+    assert.ok(person.isAdult());
   }
 }
